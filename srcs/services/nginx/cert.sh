6aace06738610a9536fd6e8e24737386aad42892
Ahmed Elaaser <ahmed.elaaser@outlook.com>
2025-08-25 14:16:58 +0200

    basic make file for docker nginx dynamic

--- srcs/services/nginx/cert.sh
+++ srcs/services/nginx/cert.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
+envsubst '${USERNAME}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default
 
-sed -i "s/my_cert/$ssl_certificate/g" /etc/nginx/sites-available/default
-sed -i "s/my_key/$ssl_certificate_key/g" /etc/nginx/sites-available/default
-sed -i "s/DOMAIN_NAME/$nginx_domain/g" /etc/nginx/sites-available/default   
-
-nginx -g "daemon off;"
+# Start nginx
+nginx -g 'daemon off;'
